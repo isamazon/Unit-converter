70ad1b7ce2a0f78d196da3a7df41e8022e80d7b6
Isaiah Mazon <37008425+isamazon@users.noreply.github.com>
2020-12-03 15:48:31 -0800

    Changing the entire conversion process

---
+++ src/refactored.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const view = {
+  button: document.querySelector("button"),
+  form: document.querySelector("form"),
+  conversionType: document.querySelector("select"),
+  result: document.querySelector(".results"),
+  input: document.querySelector("input"),
+};
+console.log(view.button);
--- src/time.js
+++ src/time.js
@@ -1,131 +1,22 @@
 "use strict";
+// Get all values and put it into a object
+const view = {
+  button: document.querySelector(".convertbtn"),
+  form: document.querySelector("form"),
+  conversionType1: document.getElementById("cfrom"),
+  conversionType2: document.getElementById("cto"),
+  result: document.querySelector(".results"),
+  input: document.querySelector("input"),
+};
 
-// create a button variable
-const convertBtn = document.querySelector(".convertbtn");
-// Make the dropdown menus variables
-const cfromMenu = document.getElementById("cfrom");
-const cToMenu = document.getElementById("cto");
+// Put all form option values and put it into an object
 
-// Now make all of the dropdown menu items into variables
-const seconds = (document.getElementById("cfrom").value = "seconds"),
-  minutes = (document.getElementById("cfrom").value = "minutes"),
-  hours = (document.getElementById("cfrom").value = "hours"),
-  days = (document.getElementById("cfrom").value = "days"),
-  weeks = (document.getElementById("cfrom").value = "weeks"),
-  months = (document.getElementById("cfrom").value = "months"),
-  years = (document.getElementById("cfrom").value = "years");
-// Turn the result message into a variable
-const result = document.querySelector(".results");
-
-document.querySelector(".convertbtn").addEventListener("click", function () {
-  // We first want to create objects for the imput values
-  let input = document.getElementById("cnumber").value;
-
-  /*                    SECONDS TO (...)                    */
-  // Seconds to minutes
-  if (cfromMenu.value === seconds && cToMenu.value === minutes) {
-    let sum = input / 60;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} = ${sum} ${cToMenu.value} = 
-    `;
-    // Seconds to hours
-  } else if (cfromMenu.value === seconds && cToMenu.value === hours) {
-    let sum = input / 3600;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} equals to ${sum} ${cToMenu.value}`;
-    // Seconds to days
-  } else if (cfromMenu.value === seconds && cToMenu.value === days) {
-    let sum = input / 86400;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} = ${sum} ${cToMenu.value}`;
-    // Seconds to weeks
-  } else if (cfromMenu.value === seconds && cToMenu.value === weeks) {
-    let sum = input / 604800;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} equals to ${sum} ${cToMenu.value}`;
-    // Seconds to months
-  } else if (cfromMenu.value === seconds && cToMenu.value === months) {
-    let sum = input / 2.628e6;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} equals to ${sum} ${cToMenu.value}`;
-    // Seconds to Years
-  } else if (cfromMenu.value === seconds && cToMenu.value === years) {
-    let sum = input / 3.154e7;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} = ${sum} ${cToMenu.value}`;
-    /*                    ..................                    */
-
-    /*                    Minutes to (...)                    */
-    // Minutes to seconds
-  } else if (cfromMenu.value === minutes && cToMenu.value === seconds) {
-    let sum = input * 60;
-    let intoHours = 60 / input;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} = ${sum} ${cToMenu.value} `;
-    // Minutes to hours
-  } else if (cfromMenu.value === minutes && cToMenu.value === hours) {
-    let sum = input / 60;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} = ${sum} ${cToMenu.value} `;
-    // Minutes to days
-  } else if (cfromMenu.value === minutes && cToMenu.value === days) {
-    let sum = input / 1440;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} = ${sum} ${cToMenu.value} `;
-    // Minutes to weeks
-  } else if (cfromMenu.value === minutes && cToMenu.value === weeks) {
-    let sum = input / 10080;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} = ${sum} ${cToMenu.value} `;
-    // Minutes to months
-  } else if (cfromMenu.value === minutes && cToMenu.value === months) {
-    let sum = input / 10080;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} = ${sum} ${cToMenu.value} `;
-    // Minutes to Years
-  } else if (cfromMenu.value === minutes && cToMenu.value === years) {
-    let sum = input / 525600;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} = ${sum} ${cToMenu.value} `;
-    /*                    ................                    */
-    /*                    Hours to (...)                    */
-    // Hours to seconds
-  } else if (cfromMenu.value === hours && cToMenu.value === seconds) {
-    let sum = input * 3600;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} = ${sum} ${cToMenu.value} `;
-    // Hours to minutes
-  } else if (cfromMenu.value === hours && cToMenu.value === minutes) {
-    let sum = input * 60;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} = ${sum} ${cToMenu.value} `;
-    // Hours to hours
-  } else if (cfromMenu.value === hours && cToMenu.value === hours) {
-    let sum = input * 1;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} = ${sum} ${cToMenu.value} `;
-    // Hours to days
-  } else if (cfromMenu.value === hours && cToMenu.value === days) {
-    let sum = input / 24;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} = ${sum} ${cToMenu.value} `;
-    // Hours to weeks
-  } else if (cfromMenu.value === hours && cToMenu.value === weeks) {
-    let sum = input / 168;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} = ${sum} ${cToMenu.value} `;
-    // Hours to months
-  } else if (cfromMenu.value === hours && cToMenu.value === months) {
-    let sum = input / 730;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} = ${sum} ${cToMenu.value} `;
-    // Hours to years
-  } else if (cfromMenu.value === hours && cToMenu.value === years) {
-    let sum = input / 8760;
-    result.classList.remove("hidden");
-    result.textContent = `${input} ${cfromMenu.value} = ${sum} ${cToMenu.value} `;
-    /*                   ................                   */
-  }
+view.conversionType1.addEventListener("change", (e) => {
+  view.result.textContent = `You chose ${e.target.value}`;
 });
 
-// Add a Results in other forms options
+const secondsTo = {
+    SecToMin = view.input / 60, 
+    SecToHour = view.input /3600
+
+}
